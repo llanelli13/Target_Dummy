7b760e0e3ce0378b0ae08c142ffb35e25a651ef6
JaTadd <jean-alexis.taddei@efrei.net>
2025-01-09 22:38:25 +0100

    del lines

--- backend/routes/shotSequenceRoutes.js
+++ backend/routes/shotSequenceRoutes.js
@@ -5,6 +5,10 @@ const shotSequenceController = require('../controllers/shotSequenceController');
 router.post('/', shotSequenceController.createShotSequence);
 router.get('/', shotSequenceController.getShotSequences);
 
+router.get('/user/:userId', shotSequenceController.getHistory); // Historique d'un utilisateur
+router.get('/:id', shotSequenceController.getShotSequenceById); // Récupérer une séquence par ID
+router.put('/:id', shotSequenceController.modifyShotSequence); // Modifier une séquence
+router.delete('/:id', shotSequenceController.deleteShotSequence); // Supprimer une séquence
+
 module.exports = router;
 
-// addShotSequence
