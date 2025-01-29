a5801928570ff9318bf086bb08398786e9c81d17
JaTadd <jean-alexis.taddei@efrei.net>
2025-01-29 21:07:10 +0100

    slider arme choix

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,12 +1,13 @@
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 const SearchBar = ({ filterType, onTypeChange, customDesign }) => {
   const { t } = useTranslation("armory");
-  const containerClass = customDesign || "flex items-center space-x-2 bg-primaryPale p-2 rounded-full w-1/2";
+  const containerClass =
+    customDesign ||
+    "flex items-center space-x-2 bg-primaryPale p-2 rounded-full w-1/2";
 
   return (
     <div className={containerClass}>
-
       {/* Liste déroulante pour filtrer par type */}
       <select
         value={filterType}
@@ -14,15 +15,15 @@ const SearchBar = ({ filterType, onTypeChange, customDesign }) => {
         className="bg-primaryBrown text-white rounded-full px-2 py-2 focus:outline-none"
       >
         <option value="">{t("type_filter")}</option>
-        <option value="Fusil d'assaut">{t('assault_rifle')}</option>
-        <option value="Arme de poing">{t('pistol')}</option>
-        <option value="Sniper">{t('sniper')}</option>
+        <option value="Fusil d'assaut">{t("assault_rifle")}</option>
+        <option value="Pistolet">{t("pistol")}</option>
+        <option value="Fusil de précision">{t("sniper")}</option>
       </select>
 
       {/* Champ de recherche */}
       <input
         type="text"
-        placeholder={t('search_bar')}
+        placeholder={t("search_bar")}
         className="w-full bg-primaryBrown placeholder-white focus:outline-none rounded-full px-2 py-2"
       />
     </div>
@@ -30,4 +31,3 @@ const SearchBar = ({ filterType, onTypeChange, customDesign }) => {
 };
 
 export default SearchBar;
-
